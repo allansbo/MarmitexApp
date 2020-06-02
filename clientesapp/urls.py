70a3b11ae868e7904a5c12760ba2189a6531b664
allansbo <38400010+allansbo@users.noreply.github.com>
2020-06-01 22:44:17 -0300

    Render do bd e add dados ao bd

--- clientesapp/urls.py
+++ clientesapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.clientslist, name='clients-list'),
-    path('yourname/<str:name>', views.yourname, name='your-name')
+    path('editar-cliente/<int:id>', views.clientedit, name="client-edit"),
+    path('adicionar-cliente/', views.clientadd, name="client-add"),
+    path('yourname/<str:name>', views.yourname, name='your-name'),
 ]
--- clientesapp/views.py
+++ clientesapp/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
+from .models import Client
+from .forms import clientAddForm
+
+
 # Create your views here.
+def clientslist(request):
+    listaclientes = Client.objects.all()
+    return render(request, 'clientesapp/clientes.html', {'listaclientes': listaclientes})
+
+
+def clientedit(request, id):
+    editarcliente = get_object_or_404(Client, pk=id)
+    form = clientAddForm(instance=editarcliente)
+
+    if request.method == 'POST':
+        form = clientAddForm(request.POST, instance=editarcliente)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return render(request, 'clientesapp/editar-cliente.html', {'form': form, 'editarcliente': editarcliente})
+    else:
+        return render(request, 'clientesapp/editar-cliente.html', {'form': form, 'editarcliente': editarcliente})
+
+
+def clientadd(request):
+    if request.method == 'POST':
+        form = clientAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = clientAddForm()
+        return render(request, 'clientesapp/adicionar-cliente.html', {'form': form})
+
+
 def helloWorld(request):
     return HttpResponse('Hello World!')
 
+
 def yourname(request, name):
     return render(request, 'clientesapp/yourname.html', {'name': name})
-
-def clientslist(request):
-    return render(request, 'clientesapp/clientes.html')
