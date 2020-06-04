2b2892f76f4cf4ab22fe7bdf889ce9c472505cd4
allansbo <38400010+allansbo@users.noreply.github.com>
2020-06-03 22:56:06 -0300

    autenticação e delete

--- clientesapp/urls.py
+++ clientesapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.clientslist, name='clients-list'),
     path('editar-cliente/<int:id>', views.clientedit, name="client-edit"),
+    path('deletar-cliente/<int:id>', views.clientdelete, name="client-delete"),
     path('adicionar-cliente/', views.clientadd, name="client-add"),
     path('yourname/<str:name>', views.yourname, name='your-name'),
 ]
--- clientesapp/views.py
+++ clientesapp/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib import messages
 
 from .models import Client
 from .forms import clientAddForm
 
 
 # Create your views here.
+@login_required()
 def clientslist(request):
     listaclientes = Client.objects.all()
     return render(request, 'clientesapp/clientes.html', {'listaclientes': listaclientes})
 
 
+@login_required()
 def clientedit(request, id):
     editarcliente = get_object_or_404(Client, pk=id)
     form = clientAddForm(instance=editarcliente)
@@ -26,6 +30,15 @@ def clientedit(request, id):
         return render(request, 'clientesapp/editar-cliente.html', {'form': form, 'editarcliente': editarcliente})
 
 
+@login_required()
+def clientdelete(request, id):
+    deletarcliente = get_object_or_404(Client, pk=id)
+    deletarcliente.delete()
+    messages.info(request, 'O registro foi excluído com sucesso!')
+    return redirect('/')
+
+
+@login_required()
 def clientadd(request):
     if request.method == 'POST':
         form = clientAddForm(request.POST)
@@ -37,9 +50,11 @@ def clientadd(request):
         return render(request, 'clientesapp/adicionar-cliente.html', {'form': form})
 
 
+@login_required()
 def helloWorld(request):
     return HttpResponse('Hello World!')
 
 
+@login_required()
 def yourname(request, name):
     return render(request, 'clientesapp/yourname.html', {'name': name})
